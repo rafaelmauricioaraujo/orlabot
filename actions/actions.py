708f38c8adae792e7d8f41c93fb9e289a8a424e5
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2021-02-07 14:11:34 -0300

    database configuration and retrive data from database to chat

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,32 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from pymongo import MongoClient
+
+
+class getProjects(Action):
+
+    def name(self) -> Text:
+        return "action_get_projects"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        client = MongoClient("mongodb+srv://rasa:Y3XMzsBIO5i1N9Re@cluster0.tpni9.mongodb.net/orla?retryWrites=true&w=majority")
+        db = client.orla
+        cursor = db.project.find({})
+        results = [doc for doc in cursor]
+        message = ''
+        for result in results:
+            print('result.name: ', result['name'])
+            message += result['name'] + ', '
+
+        dispatcher.utter_message(text=message)
+        client.close
+        return []
